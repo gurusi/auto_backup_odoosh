9169edb20636b484f0c43e1d1937c4f5a99334b1
Edi Šuc <edi@guru.si>
2023-05-12 14:03:11 +0200

    first commit

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Technical',
-    'version': '14.0.0.1.0',
+    'version': '15.0.0.1.0',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
